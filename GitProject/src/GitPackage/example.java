fa17f27fd540d120f3fd7b267336abd50f51ed7e
pravinkamble-07 <pravinkamble305@gmail.com>
2023-11-06 19:44:42 +0530

    Adding 1 more line

--- GitProject/src/GitPackage/example.java
+++ GitProject/src/GitPackage/example.java
@@ -5,6 +5,7 @@ public class example {
 	public static void main(String[] args) {
 		System.out.println("Practies make perfact");
 		System.out.println("Hello Class");
+		System.out.println("Ok bring it");
 	}
 
 }
